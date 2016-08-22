fcf7bff5d291c5c7019c6c398078eb6b858c9c7a
Gabriel Titerlea <titergab@yahoo.com>
2016-08-22 11:58:01 +0300

    Running

--- src/main/java/io/gab/Authenticator.java
+++ src/main/java/io/gab/Authenticator.java
@@ -3,12 +3,30 @@ package io.gab;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.crypto.hash.Sha512Hash;
+import org.apache.shiro.util.ByteSource;
 
 public class Authenticator implements org.apache.shiro.authc.Authenticator {
 
   @Override
   public AuthenticationInfo authenticate(AuthenticationToken token) throws AuthenticationException {
-    return null;
+    String user = (String) token.getPrincipal();
+    char[] password = (char[]) token.getCredentials();
+    
+    // Generate salt.
+    byte[] salt = "salt".getBytes();
+    
+    String realmName = "defaultRealm";
+    
+    // Fake auth
+    if ("admin".equals(user) && "admin".equals(password.toString())) {
+      ByteSource credentialsSalt = new Sha512Hash(password, salt, 128);
+      SimpleAuthenticationInfo authInfo = new SimpleAuthenticationInfo(user, password, credentialsSalt, realmName);
+      return authInfo;        
+    } else {
+      return null;
+    }
   }
   
 }
