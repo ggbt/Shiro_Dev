9556d27126b49cf993989c676db560a9780bf66a
Gabriel Titerlea <titergab@yahoo.com>
2016-08-22 09:29:22 +0300

    Auth realm

---
+++ src/main/java/io/gab/AuthenticatingRealm.java
@@ -0,0 +1,16 @@
+package io.gab;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAccount;
+
+public class AuthenticatingRealm extends org.apache.shiro.realm.AuthenticatingRealm {
+
+  @Override
+  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+    SimpleAccount account = new SimpleAccount();
+    return account;
+  }
+
+}
