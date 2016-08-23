2dfad86a65b40e8a5863f5ba01c96de1283c20bd
Gabriel Titerlea <titergab@yahoo.com>
2016-08-23 15:14:48 +0300

    Done with buji

--- src/main/java/io/gab/rest/PrivateInfo.java
+++ src/main/java/io/gab/rest/PrivateInfo.java
@@ -1,14 +1,9 @@
 package io.gab.rest;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
-
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.session.Session;
-import org.apache.shiro.subject.Subject;
 
 @Path("/rest/private")
 public class PrivateInfo {
@@ -16,9 +11,9 @@ public class PrivateInfo {
   @GET
   @Path("/")
   public String get() {
-    Subject currentUser = SecurityUtils.getSubject();
+    // Subject currentUser = SecurityUtils.getSubject();
 
-    Session session = currentUser.getSession();
+    // Session session = currentUser.getSession();
 
     return "private";
   }
@@ -28,7 +23,7 @@ public class PrivateInfo {
   public void submit(final @Suspended AsyncResponse response) {
     new Thread() {
       public void run() {
-        Subject currentUser = SecurityUtils.getSubject();
+        //Subject currentUser = SecurityUtils.getSubject();
         
         
         response.resume("private-async");
